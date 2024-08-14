d1ebc7c111215047cecc063d0515964f048390cf
Abulfida Akmal <echo.ablfda@gmail.com>
2024-08-14 10:19:07 +0800

    final

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import { addDynamicIconSelectors } from "@iconify/tailwind";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "18px",
+    },
+    extend: {
+      colors: {
+        primary: "#cbd5e1",
+        secondary: "#4338ca",
+        dark: "#111827",
+      },
+    },
+  },
+  plugins: [addDynamicIconSelectors()],
+};
